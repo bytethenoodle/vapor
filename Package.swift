bd224bb87097023e9c124d70f835dae57af6bd32
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-08-02 03:00:16 +0800

    updated sources

--- Package.swift
+++ Package.swift
@@ -24,22 +24,22 @@ let package = Package(
     ],
     dependencies: [
         // Core vapor transport layer
-        .Package(url: "https://github.com/vapor/engine.git", majorVersion: 2),
+        .Package(url: "https://github.com/bytethenoodle/engine.git", majorVersion: 2),
 
         // Console protocol and implementation for powering command line interface.
         .Package(url: "https://github.com/bytethenoodle/console.git", majorVersion: 2),
 
         // JSON enum wrapper around Foundation JSON
-        .Package(url: "https://github.com/vapor/json.git", majorVersion: 2),
+        .Package(url: "https://github.com/bytethenoodle/json.git", majorVersion: 2),
 
         // A type safe routing package including HTTP and TypeSafe routers.
-        .Package(url: "https://github.com/vapor/routing.git", majorVersion: 2),
+        .Package(url: "https://github.com/bytethenoodle/routing.git", majorVersion: 2),
 
         // Parses `Content-Type: multipart` as defined in RFC 2046.
         .Package(url: "https://github.com/bytethenoodle/multipart.git", majorVersion: 2),
 
         // Swift implementation of the BCrypt password hashing function
-        .Package(url: "https://github.com/vapor/bcrypt.git", majorVersion: 1),
+        .Package(url: "https://github.com/bytethenoodle/bcrypt.git", majorVersion: 1),
     ],
     exclude: [
         "Sources/Development",
